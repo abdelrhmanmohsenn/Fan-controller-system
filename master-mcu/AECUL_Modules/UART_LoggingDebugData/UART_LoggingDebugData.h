748fc14c844234e99276a005ae5a6ae83b45b274
abdelrhmanmohsenn <abdelrhmanmohsenn@gmail.com>
2024-03-13 01:23:20 +0200

    main application updated

---
+++ master-mcu/AECUL_Modules/UART_LoggingDebugData/UART_LoggingDebugData.h
@@ -0,0 +1,15 @@
+/* 
+ * File:   UART_LoggingDebugData.h
+ * Author: Mohsen
+ * Created on May 18, 2023, 4:42 PM
+ */
+
+#ifndef UART_LOGGINGDEBUGDATA_H
+#define	UART_LOGGINGDEBUGDATA_H
+
+#include "../../mcc_generated_files/eusart.h"
+
+void UART_LoggingDebugData_Send_String(uint8_t *String, uint16_t String_Length);
+
+#endif	/* UART_LOGGINGDEBUGDATA_H */
+
